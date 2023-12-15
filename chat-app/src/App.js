344d80a3babdf99cfc3cab345e976b9215ad9fab
Johan Trieloff <johan@trieloff.net>
2023-12-15 17:08:46 +0100

    Sends messges

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,17 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  const handleSend = () => {
+    setMessages([...messages, newMessage]);
+    setNewMessage('');
+  };
+
   return (
     <div className='chat-app'>
       <header>
         <h1>Group Chat</h1>
       </header>
       <div className='chat'>
-
+        {messages.map((message, index) => <p key={index}>{message}</p>)}
       </div>
       <footer>
-        <input type="text" placeholder="Type a message" />
-        <button className="send-btn">➤</button>
+        <input type="text" placeholder="Type a message" value={newMessage} onChange={e => setNewMessage(e.target.value)} />
+        <button className="send-btn" onClick={handleSend}>➤</button>
       </footer>
     </div>
   );
