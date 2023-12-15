1fc19cf786bbeacfb9c15067ddc23d4783614a6f
Johan Trieloff <johan@trieloff.net>
2023-12-15 17:04:00 +0100

    basic layout done

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,10 +1,18 @@
-
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Chat App</h1>
+    <div className='chat-app'>
+      <header>
+        <h1>Group Chat</h1>
+      </header>
+      <div className='chat'>
+
+      </div>
+      <footer>
+        <input type="text" placeholder="Type a message" />
+        <button className="send-btn">➤</button>
+      </footer>
     </div>
   );
 }
